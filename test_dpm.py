8c5c61ba1d76b77ac783adcd4bb7361e45cc088b
Alexis Vallet <alexis.vallet@gmail.com>
2014-06-11 15:03:24 +0900

    Added tests for model equality.

--- test_dpm.py
+++ test_dpm.py
@@ -7,8 +7,30 @@ import numpy.random as npr
 import featpyramid as pyr
 import cv2
 import testdata
+import copy
 
 class TestDPM(unittest.TestCase):
+    def testequals(self):
+        """ Tests that dpm equality works as expected.
+        """
+        img = cv2.imread('./data/images/source/asahina_mikuru_0.jpg')
+        labimg = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+        nbbins = (4,4,4)
+        pyramid = pyr.FeatPyramid(labimg, 
+                                  lambda img: pyr.labhistogram(img, nbbins).flatten('C')
+                                  ,64)
+        for i in range(0,100):
+            mixture = testdata.randommixture(pyramid)
+            self.assertEqual(mixture, mixture)
+            # Change coefficients at random in a root (if not empty)
+            if len(mixture.dpms) > 0:
+                rtcmixture = copy.deepcopy(mixture)
+                randdpm = rtcmixture.dpms[npr.randint(0, max(1, len(mixture.dpms)))]
+                i1, i2, i3 = map(lambda d: npr.randint(0,max(1,d)), randdpm.root.shape)
+                randdpm.root[i1,i2,i3] = randdpm.root[i1,i2,i3] + 1
+                self.assertNotEqual(mixture, rtcmixture)
+            
+
     def testvectorconversion(self):
         """ Tests that one can convert to and from vector and get the
             same thing.
