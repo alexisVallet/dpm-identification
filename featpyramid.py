Debugged some crucial stuff in the feature pyramids. Matching is now almost correct - there is probably an off by one lying around somewhere.

featpyramid.py
@@ -23,18 +23,19 @@ def computefeaturemap(image, mindimdiv, feature, featdim):
     # Compute the number of division for the height
     n = mindimdiv
     m = int(round(height * n / width))
-    jstep = width / n
-    istep = height / m
     
     featuremap = np.empty([m, n, featdim])
+    # We cut up the image into an m by n grid. To avoir rounding errors, we
+    # first compute the points of the grid, then iterate over them.
+    rowindexes = np.round(np.linspace(0, height, num=m+1)).astype(np.int32)
+    colindexes = np.round(np.linspace(0, width, num=n+1)).astype(np.int32)
 
     for i in range(0,m):
+        starti = rowindexes[i]
+        endi = rowindexes[i+1]
         for j in range(0,n):
-            # ugly stuff to minimize rounding errors
-            starti = int(round(i * height / n)) if i > 0 else 0
-            startj = int(round(j * width / m)) if j > 0 else 0
-            endi = int(round((i + 1) * height / n)) if i < m - 1 else height -1
-            endj = int(round((j + 1) * width / m)) if j < n - 1 else width - 1
+            startj = colindexes[j]
+            endj = colindexes[j+1]
             block = image[starti:endi,startj:endj]
             # Compute its feature
             featuremap[i,j,:] = feature(block)
@@ -65,7 +66,7 @@ class FeatPyramid:
                                                           interpolation=cv2.INTER_CUBIC)
                                                ,mindimdiv, feature, featdim))
 
-def colorhistogram(image, nbbins=(4,4,4), limits=((0,255),(0,255),(0,255))):
+def colorhistogram(image, nbbins=(4,4,4), limits=(0,255,0,255,0,255)):
     """ Compute a color histogram of an image in a numpy array.
     
     Arguments:
@@ -82,6 +83,7 @@ def colorhistogram(image, nbbins=(4,4,4), limits=((0,255),(0,255),(0,255))):
         An nbbins shaped numpy array containing the color histogram of the input image.
     """
     nbchannels = image.shape[2]
+
     return cv2.calcHist([image], range(0, nbchannels), None, nbbins, limits)
 
 """ Computes lab histogram of an image.

matching.py
@@ -110,8 +110,13 @@ def dpm_matching(pyramid, dpmmodel):
     # Compute the score as the maximum value in the score map, and the root position
     # as the position of this maximum.
     ri, rj = np.unravel_index(scoremap.argmax(), scoremap.shape)
+    # Since we match the template by moving its center along the image, and
+    # we expect the position of the top left corner in the rest of the code,
+    # we need to convert it back
+    rootpos = np.array((rj - (dpmmodel.root.shape[1] // 2), 
+                        ri - (dpmmodel.root.shape[0] // 2)), np.int32)
     
-    return (scoremap[ri, rj], np.array((rj, ri), np.int32), partresponses)
+    return (scoremap[ri, rj], rootpos, partresponses)
 
 def mixture_matching(pyramid, mixture):
     """ Matches a mixture model against a feature pyramid, and computes
@@ -142,14 +147,13 @@ def mixture_matching(pyramid, mixture):
     displacements = []
     score, comp, rootpos, partesponses, c = bestcomp
     # initialize at root filter size + deformations + bias
-    latvecsize = comp.root.size + 4 * (len(comp.parts) + 1) + 1
+    latvecsize = comp.root.size + 4 * (len(comp.parts)) + 1
 
     for i in range(0, len(comp.parts)):
         (partgdt, args) = gdt.gdt2D(comp.deforms[i], -partresponses[i])
         ancj, anci = np.round(2*rootpos + comp.anchors[i]).astype(np.int32)
-        print repr((anci,ancj))
-        print repr(2*rootpos)
-        print repr(comp.anchors[i])
+        print args.shape
+        print repr((anci, ancj))
         absolutepos.append(args[anci, ancj])
         displacements.append(args[anci, ancj] - 
                              np.array([anci,ancj], dtype=np.int32))
@@ -163,8 +167,8 @@ def mixture_matching(pyramid, mixture):
     # root filter
     rrows, rcols = comp.root.shape[0:2]
     latvec[0:comp.root.size] = (
-        pyramid.features[1][rootpos[0]:rootpos[0]+rrows,
-                            rootpos[1]:rootpos[1]+rcols]
+        pyramid.features[1][rootpos[1]:rootpos[1]+rrows,
+                            rootpos[0]:rootpos[0]+rcols]
     ).flatten('C')
     offset = offset + comp.root.size;
     # part filters
@@ -172,17 +176,18 @@ def mixture_matching(pyramid, mixture):
         part = comp.parts[i]
         prows, pcols = part.shape[0:2]
         uli, ulj = absolutepos[i]
-        latvec[offset:offset+part.size] = (
-            pyramid.features[0][uli:uli+prows,ulj:ulj+pcols]
-        ).flatten('C')
+        subwindow = pyramid.features[0][uli:uli+prows,ulj:ulj+pcols]
+        latvec[offset:offset+part.size] = subwindow.flatten('C')
         offset = offset + part.size
     
-    # deformation 
+    # deformation
     for i in range(0, len(comp.parts)):
-        latvec[offset:offset+4] = comp.deforms[i]
+        dx = displacements[i][1]
+        dy = displacements[i][0]
+        latvec[offset:offset+4] = -np.array([dx, dy, dx**2, dy**2])
         offset = offset + 4
 
     # bias
-    latvec[len(latvec) - 1] = comp.bias
+    latvec[len(latvec) - 1] = 1
 
     return (score, c, latvec)

test_featpyramid.py
@@ -9,15 +9,17 @@ class TestFeatPyramid(unittest.TestCase):
     def test_initialization_labhist(self):
         """ Tests whether the histograms of blocks add up to the full image histogram.
         """
-        img = cv2.imread('./data/images/source/asahina_mikuru_0.jpg')
-        labimg = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+        bgrimg = cv2.imread('./data/images/source/asahina_mikuru_0.jpg')
+        labimg = cv2.cvtColor(bgrimg.astype(np.float32) / 255, cv2.COLOR_BGR2LAB)
         nbbins = (4,4,4)
         pyramid = pyr.FeatPyramid(labimg,
                                   lambda img: pyr.labhistogram(img, nbbins).flatten('C')
                                   ,64)
-        fullhist = pyr.labhistogram(labimg, nbbins).flatten('C')
-        pyramidsum = np.sum(pyramid.features[0], (0,1))
-        np.testing.assert_almost_equal(fullhist, pyramidsum)
+        fullhistsum = pyr.labhistogram(labimg, nbbins).sum()
+        featzerosum = pyramid.features[0].sum()
+
+        self.assertEqual(fullhistsum, featzerosum)
+        self.assertEqual(fullhistsum, np.prod(bgrimg.shape[0:2]))
         
 
 if __name__ == "__main__":

test_matching.py
@@ -46,7 +46,7 @@ class TestMatching(unittest.TestCase):
         """ Test single component matching a test image with a random dpm
         """
         img = cv2.cvtColor(
-            cv2.imread("data/images/source/asahina_mikuru_2.jpg"),
+            cv2.imread("data/images/source/asahina_mikuru_2.jpg").astype(np.float32) / 255,
             cv2.COLOR_BGR2LAB
         )
         nbbins = (4,4,4)
@@ -73,7 +73,7 @@ class TestMatching(unittest.TestCase):
 
     def test_mixture_matching(self):
         img = cv2.cvtColor(
-            cv2.imread("data/images/source/asahina_mikuru_2.jpg"),
+            cv2.imread("data/images/source/asahina_mikuru_2.jpg").astype(np.float32) / 255,
             cv2.COLOR_BGR2LAB
         )
         nbbins = (4,4,4)
@@ -87,19 +87,22 @@ class TestMatching(unittest.TestCase):
         root1 = pyramid.features[1]
         part1_1 = pyramid.features[0][10:20,10:20]
         part2_1 = pyramid.features[0][20:30,20:30]
-        model1 = dpm.DPM(root1, [part1_1, part2_1], 
+        model1 = dpm.DPM(root1, [part1_1, part2_1],
                          [np.array([10,10]), np.array([20,20])],
                          [np.array([0,0,0.1,0.1])] * 2, 1)
-        root2 = pyramid.features[1][10:30,10:30]
-        part1_2 = pyramid.features[0][0:20,0:20]
-        part2_2 = pyramid.features[0][20:40,20:40]
+        root2 = pyramid.features[1][5:15,5:15]
+        part1_2 = pyramid.features[0][10:15,10:15]
+        part2_2 = pyramid.features[0][15:30,15:30]
         model2 = dpm.DPM(root2, [part1_2, part2_2],
-                         [np.array([0,0]), np.array([20,20])],
+                         [np.array([0,0]), np.array([10,10])],
                          [np.array([0,0,0.1,0.1])] * 2, 1)
         mixture = dpm.Mixture([model1,model2])
         (score, c, latvec) = matching.mixture_matching(pyramid, mixture)
         # Check that the latent vector gives the proper score
-        self.assertAlmostEqual(score, numpy.vdot(latvec, mixture.dpms[c].tovector()))
+        modelvector = mixture.dpms[c].tovector()
+        print repr(modelvector.sum())
+        print repr(latvec.sum())
+        self.assertAlmostEqual(score, np.vdot(latvec, modelvector))
 
 if __name__ == "__main__":
     unittest.main()